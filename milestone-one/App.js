00175cc3c83fc4571463c4b1c3eeb4a3397a530d
Kaaya <masontdunbar@live.com>
2024-03-21 20:51:29 +1000

    Added Styling and A Box around the items

--- milestone-one/App.js
+++ milestone-one/App.js
@@ -1,13 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
+import { cloneElement } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>My To-do List</Text>
-      <Text style={styles.item}>Item 1</Text>
-      <Text style={styles.item}>Item 2</Text>
-      <Text style={styles.item}>Item 3</Text>
+      <View style={styles.itemBox}>
+        <Text style={styles.item}>Clean my Car</Text>
+        <Text style={styles.item}>Buy a Gift for Mother</Text>
+        <Text style={styles.item}>Get a Haircut</Text>
+        <Text style={styles.item}>Buy Groceries</Text>
+        <Text style={styles.item}>Pay my Bills</Text>
+      </View>
       <Text style={styles.button}>Add New To-do Item</Text>
       <StatusBar style="auto" />
     </View>
@@ -17,8 +22,43 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f2f2f2',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+  },
+  title:{
+    textAlign: 'center',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 40,
+    top: 10,
+    position: 'absolute',
+  },
+  itemBox: {
+    width: '100%',
+    height: '80%',
+    borderWidth: 5,
+    borderColor: 'black',
+    borderRadius: 10,
+    padding: 10,
+    marginBottom: 10,
+  },
+  item: {
+    fontSize: 16,
+    borderWidth: 1,
+    minHeight: '15%',
+    backgroundColor: 'green',
+    padding: 10,
+    marginBottom: 5,
+    color: 'white',
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 25,
+    textAlign: 'center',
+    overflow: 'hidden',
+    color: 'white',
   },
 });
