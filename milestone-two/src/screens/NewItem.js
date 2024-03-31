abe6c356954461bc45bf089fbc36762535a8bf11
Kaaya <masontdunbar@live.com>
2024-03-31 14:03:56 +1000

    Milestone 2 - Add components to New item page

--- milestone-two/src/screens/NewItem.js
+++ milestone-two/src/screens/NewItem.js
@@ -1,9 +1,17 @@
 import { StyleSheet, Text, View } from 'react-native';
+import { IconButton } from "../components/IconButton";
+import { TextInput } from 'react-native-web';
 
 export default function NewItem () {
   return (
     <View style={styles.container}>
       <Text>New Item</Text>
+      <Text>Title</Text>
+      <TextInput placeholder="Add a Title for your task" style={styles.input}/>
+      <Text>Description</Text>
+      <TextInput placeholder="Add a Title for your task" style={styles.input}/>
+      <IconButton name="backspace-outline" label="Cancel"/>
+      <IconButton name="save-outline" label="Save"/>
     </View>
   );
 }
@@ -13,5 +21,11 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    borderWidth: 1,
+    padding: 5,
+    margin: 10,
   },
 });
